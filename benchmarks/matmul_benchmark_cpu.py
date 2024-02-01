4a82362885113092441733b069a09da746f3a5d3
Andrei Panferov <andrei@panferov.org>
2024-02-01 16:56:28 +0300

    bette cpu benchmark

--- benchmarks/matmul_benchmark_cpu.py
+++ benchmarks/matmul_benchmark_cpu.py
@@ -2,10 +2,12 @@ import os
 os.environ["OMP_NUM_THREADS"]="1"
 os.environ["MKL_NUM_THREADS"]="1"
 
+import numpy as np
+import numba
+import torch
+
 import argparse
 import time
-import torch
-torch.set_num_threads(1)
 from torch import nn
 import torch.nn.functional as F
 
@@ -116,13 +118,36 @@ if __name__ == "__main__":
         default=8,
         help="Input group size.",
     )
+    parser.add_argument(
+        "--nthreads",
+        type=int,
+        default=1,
+        help="Num threads.",
+    )
 
     args = parser.parse_args()
+    
+    numba.set_num_threads(args.nthreads)
+    torch.set_num_threads(args.nthreads)
 
     for model, layers in MODELS['fuse' if args.fuse else 'no-fuse'].items():
         dense = 0
         quant = 0
         for in_features, out_features in layers:
+            in_group_size, num_codebooks, nbits_per_codebook = args.in_group_size, args.num_codebooks, args.nbits_per_codebook
+            @numba.njit(parallel=True)
+            def numba_gemv_lut_(x, codebooks, codes_alt, scales):
+                lut = x.reshape(-1, in_group_size) @ codebooks.reshape(-1, in_group_size).T
+                lut = lut.reshape(-1, num_codebooks, 2 ** nbits_per_codebook)
+
+                output_vec = np.zeros(out_features, dtype=x.dtype)
+                for j in numba.prange(in_features//in_group_size):
+                    for i in range(out_features):
+                        for c in range(num_codebooks):
+                            output_vec[i] += lut[j, c, codes_alt[j, i, c]]
+                output_vec *= scales.flatten()
+                return output_vec
+            
             input = torch.randn((1, in_features), dtype=torch.float32)  #  [..., in_features]
             codes = pack_int_data(
                 torch.randint(2 ** args.nbits_per_codebook, (in_features // args.in_group_size, out_features, args.num_codebooks)),  #  [num_in_groups, num_out_groups, num_codebooks]
@@ -134,12 +159,14 @@ if __name__ == "__main__":
             weight = _dequantize_weight(unpack_int_data(torch.permute(codes, (1, 0, 2)), args.nbits_per_codebook), codebooks, scales).contiguous()
             
             output_ref = F.linear(input, weight)
-            output = numba_gemm_lut(input, codes, codebooks, scales, None)
+            output = numba_gemv_lut_(input.numpy(), codebooks.numpy(), codes.numpy(), scales.numpy())
             if args.log_error:
                 print(f"Relative error: {(torch.mean(torch.abs(output_ref - output)) / torch.mean(torch.abs(output_ref))).item():.2e}")
 
             dense += benchmark(lambda: F.linear(input, weight, out=output_ref), args.warmup_iters, args.benchmark_iters)
-            quant += benchmark(lambda: numba_gemm_lut(input, codes, codebooks, scales, None), args.warmup_iters, args.benchmark_iters)
+            input, codebooks, codes, scales = input.numpy(), codebooks.numpy(), codes.numpy(), scales.numpy()
+            quant += benchmark(lambda: numba_gemv_lut_(input, codebooks, codes, scales), args.warmup_iters, args.benchmark_iters)
+            
         print(f"{model}: Dense forward = {dense * 1e3:.2f} ms")
         print(f"{model}: Quant forward = {quant * 1e3:.2f} ms")
         print(f"{model}: Speedup relative to dense = {(dense / quant):.3f}")
