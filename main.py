ebe8ecefe3d4e0e4c4281fed08cf12b2fb658ca4
Andrei Panferov <andrei@panferov.org>
2024-02-29 14:58:58 +0100

    model_type check

--- main.py
+++ main.py
@@ -248,7 +248,8 @@ def quantize_aq(model: PreTrainedModel, dataloader: Iterable, args: Namespace):
             print("PREPARING TO FINETUNE")
             print(layer)
             layer = layer.to(dtype=torch.float32)
-            layer.config._pre_quantization_dtype = torch.float32
+            if model.config.model_type == "mistral":
+                layer.self_attn.config._pre_quantization_dtype = torch.float32
             with using_tf32(enabled=True):
                 layer = finetune_groupwise(layer=layer, inps=inps, outs=outs, args=args, **forward_args)
             layer = layer.to(dtype=layer_dtype_original)
