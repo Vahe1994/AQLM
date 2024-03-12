20add601010cb0ec485f0c52fc1116640ae4f4c9
Andrei Panferov <andrei@panferov.org>
2024-03-12 21:41:35 +0100

    cleaner cc check

--- inference_lib/src/aqlm/inference_kernels/cuda_kernel.cu
+++ inference_lib/src/aqlm/inference_kernels/cuda_kernel.cu
@@ -51,7 +51,7 @@ __global__ void Code1x16MatVec(
           : "l"((void*) &codebook[enc[i]])
         );
         if constexpr (use_bfloat16) {
-        #if defined(__CUDACC__) && (!defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800) || defined(_NVHPC_CUDA))
+        #if !defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800)
           nv_bfloat162* a = reinterpret_cast<nv_bfloat162*>(&dec);
           nv_bfloat162* b = reinterpret_cast<nv_bfloat162*>(&sh_b[b_sh_rd]);
           nv_bfloat162 res2 = {};
@@ -140,7 +140,7 @@ __global__ void Code2x8MatVec(
       #pragma unroll
       for (int i = 0; i < 8; i++) {
         if constexpr (use_bfloat16) {
-        #if defined(__CUDACC__) && (!defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800) || defined(_NVHPC_CUDA))
+        #if !defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800)
           nv_bfloat162* a0 = reinterpret_cast<nv_bfloat162*>(&sh_code0[8 * enc[2 * i + 0] + lane]);
           nv_bfloat162* a1 = reinterpret_cast<nv_bfloat162*>(&sh_code1[8 * enc[2 * i + 1] + lane]);
           nv_bfloat162*  b = reinterpret_cast<nv_bfloat162*>(&sh_b[b_sh_rd]);
@@ -195,14 +195,12 @@ void  code1x16_matvec_cuda(
   int prob_k,
   bool use_bfloat16
 ) {
-  #if !(defined(__CUDACC__) && (!defined(__CUDA_ARCH__) || (__CUDA_ARCH__ >= 800) || defined(_NVHPC_CUDA)))
+  #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 800)
   if (use_bfloat16) {
     throw c10::TypeError(
       {__func__, __FILE__, static_cast<uint32_t>(__LINE__)},
       c10::str(
-        "You're trying to run AQLM with bfloat16 on a GPU with compute capability ",
-        __CUDA_ARCH__,
-        ", and you need at least 8.0 to do that. Use torch.float16 instead."
+        "You're trying to run AQLM with bfloat16 on a GPU with low compute capability. Use torch.float16 instead."
       )
     );
   } 
@@ -250,6 +248,17 @@ void  code2x8_matvec_cuda(
   int prob_k,
   bool use_bfloat16
 ) {
+  #if defined(__CUDA_ARCH__) && (__CUDA_ARCH__ < 800)
+  if (use_bfloat16) {
+    throw c10::TypeError(
+      {__func__, __FILE__, static_cast<uint32_t>(__LINE__)},
+      c10::str(
+        "You're trying to run AQLM with bfloat16 on a GPU with low compute capability. Use torch.float16 instead."
+      )
+    );
+  } 
+  #endif
+
   int sms;
   cudaDeviceGetAttribute(&sms, cudaDevAttrMultiProcessorCount, 0);
   int waves = 0;
