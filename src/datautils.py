6f8dc585e7c0b9979ecf168c3f04c7504eca1149
Your Name <justheuristic@gmail.com>
2024-04-24 18:23:01 +0300

    group texts

--- src/datautils.py
+++ src/datautils.py
@@ -1,6 +1,7 @@
 import os
 import random
-from typing import Optional
+from itertools import chain
+from typing import Optional, Sequence
 
 import datasets
 import numpy as np
@@ -249,3 +250,21 @@ def get_loaders(
 
     print(f"Loaded data from {name}; {len(data)=} sequences")
     return data
+
+
+def group_texts(examples: Sequence[Sequence[int]], block_size: int):
+    """Group training examples together and split them into blocks of up to block_size tokens"""
+    # based on https://github.com/huggingface/transformers/blob/main/examples/pytorch/language-modeling/run_clm.py
+    # Concatenate all texts.
+    concatenated_examples = {k: list(chain(*examples[k])) for k in examples.keys()}
+    total_length = len(concatenated_examples[list(examples.keys())[0]])
+    # We drop the small remainder, and if the total_length < block_size  we exclude this batch and return an empty dict.
+    # We could add padding if the model supported it instead of this drop, you can customize this part to your needs.
+    total_length = (total_length // block_size) * block_size
+    # Split by chunks of max_len.
+    result = {
+        k: [t[i : i + block_size] for i in range(0, total_length, block_size)]
+        for k, t in concatenated_examples.items()
+    }
+    result["labels"] = result["input_ids"].copy()
+    return result
