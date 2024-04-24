654e6f0c20d8f3d92025ed3b7a86d80c0f3eace2
Your Name <justheuristic@gmail.com>
2024-04-24 16:56:59 +0300

    bs=4

--- finetune_fsdp.py
+++ finetune_fsdp.py
@@ -245,7 +245,7 @@ if __name__ == "__main__":
     input_ids = torch.arange(4 * 2048).reshape(-1, 2048).to(device)
     for i in tqdm(range(100)):
         with torch.cuda.amp.autocast(enabled=True, dtype=torch.bfloat16):
-            y = quantized_model(input_ids)
+            y = base_model(input_ids)
         y.logits.norm().backward()
     if args.wandb:
         assert has_wandb, "`wandb` not installed, try pip install `wandb`"
