af06a094210808584b18329eb116c2dde12614b2
Your Name <justheuristic@gmail.com>
2024-04-25 14:04:00 +0300

    hardcode batch size

--- finetune_fsdp.py
+++ finetune_fsdp.py
@@ -157,12 +157,6 @@ def add_data_args(parser: argparse.ArgumentParser):
         help="Number of CPU workers for preprocessing; overrides num_workers",
     )
     parser.add_argument(
-        "--preprocessing_batch_size",
-        type=int,
-        default=32,
-        help="Number of training sequences pre-processed together ",
-    )
-    parser.add_argument(
         "--eval_datasets",
         nargs="+",
         type=str,
@@ -257,7 +251,7 @@ def prepare_training_dataset(args: argparse.Namespace, tokenizer: transformers.P
     tokenized_dataset = dataset.map(
         lambda examples: tokenizer(examples[text_column_name]),
         batched=True,
-        batch_size=args.preprocessing_batch_size,
+        batch_size=32,  # reduce batch size for parallel processing with very long texts
         num_proc=args.preprocessing_num_workers if args.preprocessing_num_workers is not None else args.num_workers,
         remove_columns=list(dataset.column_names),
         load_from_cache_file=not args.overwrite_cache,
@@ -266,7 +260,6 @@ def prepare_training_dataset(args: argparse.Namespace, tokenizer: transformers.P
     lm_dataset = tokenized_dataset.map(
         partial(group_texts, block_size=args.model_seqlen, add_labels=False),
         batched=True,
-        batch_size=args.preprocessing_batch_size,
         num_proc=args.preprocessing_num_workers if args.preprocessing_num_workers is not None else args.num_workers,
         load_from_cache_file=not args.overwrite_cache,
         desc=f"Grouping texts in chunks of {args.model_seqlen}",
