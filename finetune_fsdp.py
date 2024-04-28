be4b7a7b7b1f159d16f5a2d7a3dcfcd1325c38a1
Your Name <justheuristic@gmail.com>
2024-04-29 00:06:07 +0300

    native amp

--- finetune_fsdp.py
+++ finetune_fsdp.py
@@ -349,7 +349,8 @@ def load_quantized_model(args: argparse.Namespace, device: torch.device) -> Full
     if args.amp_dtype is not None:
         ignored_classes = (IntCodes,) + tuple(transformers.pytorch_utils.ALL_LAYERNORM_LAYERS)
         mixed_precision = MixedPrecision(
-            param_dtype=args.amp_dtype, reduce_dtype=args.amp_dtype, _module_classes_to_ignore=ignored_classes)
+            param_dtype=args.amp_dtype, reduce_dtype=args.amp_dtype, _module_classes_to_ignore=ignored_classes
+        )
     return FullyShardedDataParallel(
         quantized_model,
         auto_wrap_policy=lambda module, recurse, **_: recurse or isinstance(module, blocks_to_wrap),
