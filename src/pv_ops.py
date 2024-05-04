095c19e39ade1236edb2691367030b838974eaa6
Denis Mazur <denismazur8@gmail.com>
2024-05-05 01:56:40 +0300

    pv ops, basic docstrings

--- src/pv_ops.py
+++ src/pv_ops.py
@@ -96,56 +96,100 @@ def verify_dequantized_model(dequantized_model: nn.Module, master_parameters: di
 
 class StraightThroughAdamW(torch.optim.AdamW):
     """
-    A wrapper for any PyTorch optimizer that enables updates on quantized parameters
-
+    A wrapper for a PyTorch optimizer that enables updates on quantized parameters
+    :param update_non_quantized_params: how to update parameters that are not directly linked to a QuantizedWeight.
+        This may include biases, embeddings/heads, normalization layers or parts of the model that were not quantized.
+    :param update_codebooks_and_scales: how to update continuous params of QuantizedWeight: codebooks and scales.
+        This should be a dictionary of optimizer kwargs. In the latter case, these
+            keyword arguments will be used when configuring optimizer for this specific parameter group
+    :param update_codes: how to update codes in each QuantizedWeight with beam search and straight-through grad.
+        This should be a dictionary of configurations, similar to
+    :param max_code_change_per_step: max portion of discrete code groups that can be updated; only affects codes
+    :param beam_size: beam search width used only when updating codes. See beam_size in aq.py
+    :param stochastic_rounding_tau: if above 0, use stochastic rounding with this temperature. See aq.py
     """
     def __init__(self,
                  named_dequantized_params: Dict[str, nn.Parameter],
                  named_master_params: Dict[str, Union[QuantizedWeight, nn.Parameter]],
                  *,
+                 update_non_quantized_parameters: Optional[dict] = None,
+                 update_codebooks_and_scales: Optional[dict] = None,
+                 update_codes: Optional[dict] = None,
                  max_code_change_per_step: float,
                  beam_size: int,
                  stochastic_rounding_tau: float = 0,
                  delta_dtype: Optional[torch.dtype] = None,
                  **kwargs):
-        optimized_params = self._select_optimized_parameters(named_dequantized_params, named_master_params, delta_dtype)
-        super().__init__(list(optimized_params.values()), **kwargs)
-        self._link_parameters_in_optimizer_state(optimized_params, named_dequantized_params, named_master_params)
+
+        non_quantized_params, quantized_params, quantized_representation_params = self._select_optimized_parameters(
+            named_dequantized_params, named_master_params, delta_dtype)
+        param_groups = []
+        all_optimized_params = dict()
+        if update_non_quantized_parameters is not None:
+            param_groups.append(dict(params=list(non_quantized_params.values()), **update_non_quantized_parameters))
+            all_optimized_params.update(non_quantized_params)
+        if update_codebooks_and_scales is not None:
+            param_groups.append(dict(params=list(quantized_representation_params.values()), **update_codebooks_and_scales))
+        if update_codes is not None:
+            param_groups.append(dict(params=list(quantized_params.values()), **update_codes))
+
+        super().__init__(param_groups, **kwargs)
+        self._link_parameters_in_optimizer_state(all_optimized_params, named_dequantized_params, named_master_params)
+        self.should_update_non_quantized_parameters = update_non_quantized_parameters is not None
+        self.should_update_codebooks_and_scales = update_codebooks_and_scales is not None
+        self.should_update_codes = update_codes is not None
         self.max_code_change_per_step = max_code_change_per_step
         self.stochastic_rounding_tau = stochastic_rounding_tau
         self.beam_size = beam_size
 
     def _select_optimized_parameters(self, named_dequantized_params, named_master_params, delta_dtype):
         """Choose which version of parameter to optimize: the parameter itself or a straight-through buffer"""
-        optimized_params = dict()
+        non_quantized_params, quantized_params, quantized_representation_params = dict(), dict(), dict()
         for name, param in named_dequantized_params.items():
             if name not in named_master_params:
-                optimized_params[name] = param
+                non_quantized_params[name] = param
             elif name in named_master_params and isinstance(named_master_params[name], nn.Parameter):
-                optimized_params[name] = named_master_params[name]
+                non_quantized_params[name] = named_master_params[name]
             elif name in named_master_params and isinstance(named_master_params[name], QuantizedWeight):
-                delta = torch.zeros_like(param, dtype=delta_dtype, requires_grad=True)
-                optimized_params[name] = delta  # accumulator for optimizer updates; sharded alongside FSDP
-                # note: we track delta (difference) instead of raw weight to better handle half precision
-        return optimized_params
+                if self.update_codes or self.update_codebooks_and_scales:
+                    delta = torch.zeros_like(param, dtype=delta_dtype, requires_grad=True)
+                    quantized_params[name] = delta  # accumulator for optimizer updates; sharded alongside FSDP
+                    # note: we track delta (difference) instead of raw weight to better handle half precision
+                    if self.update_codebooks_and_scales:
+                        quantized_weight = named_master_params[name]
+                        for subparam_name, subparam in quantized_weight.named_parameters():
+                            full_name = f'{name}.{subparam_name}'
+                            assert full_name not in quantized_representation_params, full_name
+                            quantized_representation_params[full_name] = full_name
+        total_params = len(set(non_quantized_params) | set(quantized_params) + set(quantized_representation_params))
+        assert total_params == len(non_quantized_params) + len(quantized_params) + len(quantized_representation_params)
+        return non_quantized_params, quantized_params, quantized_representation_params
 
-    def _link_parameters_in_optimizer_state(self, optimized_params, named_dequantized_params, named_master_params):
+    def _link_parameters_in_optimizer_state(self, all_optimized_params, named_dequantized_params, named_master_params):
         """For each optimizer parameter in state, add its name, corresponding quantized and/or offloaded parameter"""
-        for name, param in optimized_params.items():
+
+        all_quantized_representation_parameters = set()
+        for module in named_master_params.values():
+            if isinstance(module, QuantizedWeight):
+                all_quantized_representation_parameters |= set(module.parameters())
+
+        for name, param in all_optimized_params.items():
             self.state[param]['name'] = name
-            self.state[param]['param_version_that_accumulates_grad'] = named_dequantized_params[name]
             self.state[param]['is_quantized'] = isinstance(named_master_params.get(name), QuantizedWeight)
+            self.state[param]['is_part_of_quantized'] = param in all_quantized_representation_parameters
+
             if self.state[param]['is_quantized']:
                 quantized_weight = named_master_params[name]
                 assert isinstance(quantized_weight, QuantizedWeight)
                 self.state[param]['quantized_weight'] = quantized_weight
+                self.state[param]['param_version_that_accumulates_grad'] = named_dequantized_params[name]
+            elif self.state[param]['is_part_of_quantized']:
+                self.state[param]['param_version_that_accumulates_grad'] = param
+            else:  # non_quantized params, e.g. biases, layernorms, etc
+                self.state[param]['param_version_that_accumulates_grad'] = named_dequantized_params[name]
 
     def step(self, *args, **kwargs):
-        for param_group in self.param_groups:
-            for param in param_group['params']:
-                if self.state[param]['param_version_that_accumulates_grad'] is not param:
-                    param.grad = self.state[param]['param_version_that_accumulates_grad'].grad
-
+        self._propagate_grads_to_optimized_parameters()
         original_output = super().step(*args, **kwargs)
         self._update_quantized_weights()
 
@@ -154,10 +198,28 @@ class StraightThroughAdamW(torch.optim.AdamW):
                 if self.state[param]['param_version_that_accumulates_grad'] is not param:
                     param.grad = None  # deference so that previous grads can be deleted on zero_grad(set_to_none=True)
                     if self.state[param]['is_quantized']:
-                        self.state[param]['param_version_that_accumulates_grad'].data[...] = param['quantized_weight']()
+                        self.state[param]['param_version_that_accumulates_grad'].data[...] = (
+                            self.state[param]['quantized_weight']()  # de-quantize the (possibly) updated weight
+                        )
                     else:
                         self.state[param]['param_version_that_accumulates_grad'].data[...] = param.data
         return original_output
+
+    @torch.no_grad()
+    def _propagate_grads_to_optimized_parameters(self):
+        for param_group in self.param_groups:
+            for param in param_group['params']:
+                if self.state[param]['param_version_that_accumulates_grad'] is not param:
+                    param.grad = self.state[param]['param_version_that_accumulates_grad'].grad
+                    # pass gradients to straight-through update buffer or (possibly offloaded) master parameter
+        if self.should_update_codebooks_and_scales:
+            # propagate accumulated gradients from dequantized weights to quantization parameters so they can be updated
+            for param_group in self.param_groups:
+                for param in param_group['params']:
+                    if self.state[param]['is_quantized']:
+                        with torch.enable_grad():
+                            grad = self.state[param]['param_version_that_accumulates_grad'].grad
+                            self.state[param]['quantized_weight'].forward().backward(grad)
 
     @torch.no_grad()
     def _update_quantized_weights(self):
