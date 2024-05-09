68583cf06fbe6e6b6e3005fac1bf5669d3eb91c7
Your Name <justheuristic@gmail.com>
2024-05-09 18:23:02 +0300

    debugprint

--- src/pv_optimizer.py
+++ src/pv_optimizer.py
@@ -86,6 +86,10 @@ class StraightThroughAdamW(ConfigurableAdamW):
                                                 if name in all_optimized_params}
         self.dequantized_weights_by_name = {name: param for name, param in named_dequantized_params.items()
                                             if name in named_quantized_params}
+
+        print(end=f"rank={torch.distributed.get_rank()} num dequantized_weights_by_name = {len(self.dequantized_weights_by_name)}\n"
+                  f"named_quantized_params.keys={named_quantized_params.keys()}\n"
+                  f"named_dequantized_params.keys={named_dequantized_params.keys()}")
         if sharded:
             self.sharded_param_sizes_by_rank = _get_sharded_param_sizes_by_rank(named_dequantized_params)
 
@@ -160,7 +164,6 @@ class StraightThroughAdamW(ConfigurableAdamW):
         async_ops = list()
         aggregated_grads_by_name = dict()
         for name in self.ordered_quantized_weight_names:
-            print(self.dequantized_weights_by_name)
             grad = self.dequantized_weights_by_name[name].grad
             assert grad is not None
             if not self.sharded:
